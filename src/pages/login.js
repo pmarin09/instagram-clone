5ce70e633c469c12e28e714abd42ff33f938424a
Patrick Marin <patrickmarin@gmail.com>
2021-08-10 16:15:23 -0500

    Login Page Logo

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import iphone from "../images/iphone-with-profile.jpg"
+import logo from "../images/logo.png"
 export default function Login() {
     return (
         <div className ="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
-                <img src = "/images/iphone-with-profile.jpg" alt="iPhone with Instagram app" />
+                <img src = {iphone} alt="iPhone with Instagram app" />
             </div>
             <div className = "flex flex-col w-2/5">
                 <h1 className ="flex justify-center w-full">
-                    <img src="/images/logo.png" alt="Instagram" className = "mt-2 w-6/12 mb-4"/>
+                    <img src={logo} alt="Instagram" className = "mt-2 w-6/12 mb-4"/>
                   
                 </h1>
             </div>
